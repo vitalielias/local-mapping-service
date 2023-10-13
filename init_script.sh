fdcb9d5ecd4afada68b0fc1e370529387a398f30
Elias Vitali <elias.vitali@kit.edu>
2023-10-13 17:04:56 +0200

    working version

--- init_script.sh
+++ init_script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Wait for the Mapping Service to start
-sleep 15
+sleep 30
 
 # POST the mapping schemas
-curl -X POST http://mapping_service:8095/api/v1/mappingAdministration \
+curl -X POST http://mapping-service:8095/api/v1/mappingAdministration \
      -H "Content-Type: multipart/form-data" \
      -F "record=@/initializer_files/semAcqMapping.txt" \
      -F "document=@/initializer_files/test2_SEMTomoAcqToJSON_1.0.0.mapping"
